830fa45e85617eab4079e26b0671ee6eea3ce5aa
Ícaro Miranda <miranda.icarox@gmail.com>
2024-01-25 06:51:06 +0000

    First commit

---
+++ src/services/NSE.Identidade.API/Configuration/ApiConfig.cs
@@ -0,0 +1,6 @@
+﻿namespace NSE.Identidade.API.Configuration
+{
+    public static class ApiConfig
+    {
+    }
+}
---
+++ src/services/NSE.Identidade.API/Configuration/IdentityConfig.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using NSE.Identidade.API.Data;
+
+namespace NSE.Identidade.API.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIdentityConfig(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            return services;
+        }
+
+    }
+}
